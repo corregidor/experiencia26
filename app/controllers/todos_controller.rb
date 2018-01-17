73b7039c348ff21b0438ff24b19337667e9a5b05
DenisSsiTTa <denissev@gmail.com>
2018-01-17 01:04:27 -0300

    Commit

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,57 @@
+class TodosController < ApplicationController
+
+  before_action :find_todo, only: [:show, :edit, :update, :destroy, :complete]
+
+  def index
+    @todos  = Todo.all
+  end
+
+  def list
+    @todos  = Todo.all
+  end
+
+  def new
+    @todos = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo.destroy
+    respond_to do |format|
+      format.html { redirect_to todos_url }
+      format.json { head :no_content }
+    end
+  end
+
+  def complete
+    @todo.completed = true
+    @todo.save
+    redirect_to todos_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:description, :completed)
+  end
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
+
+end
